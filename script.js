0d2863a17818efddb2e080bffa2f21fa057a0139
sanb0x-lua <mr.sanbox22364@gmail.com>
2026-03-22 00:44:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
 const isMobile = /Mobi|Android/i.test(navigator.userAgent);
 
-/* фон */
+/* Фон */
 function createLines() {
     const bg = document.getElementById('bg');
     const count = isMobile ? 2 : 6;
 
-    for (let i = 0; i < count; i++) {
+    for (let i=0;i<count;i++){
         const line = document.createElement('div');
         line.classList.add('line');
-
-        line.style.left = Math.random() * 100 + 'vw';
-        line.style.animationDuration = (Math.random() * 4 + 5) + 's';
-
+        line.style.left = Math.random()*100+'vw';
+        line.style.animationDuration = (Math.random()*4+5)+'s';
         bg.appendChild(line);
-
-        setTimeout(() => line.remove(), 9000);
+        setTimeout(()=>line.remove(),9000);
     }
 }
 
-setInterval(createLines, isMobile ? 3000 : 1500);
+setInterval(createLines,isMobile?3000:1500);
 
-/* кнопки */
-document.querySelectorAll('.download-btn').forEach(btn => {
-    btn.addEventListener('click', function(e) {
+/* Кнопки скачивания */
+document.querySelectorAll('.download-btn').forEach(btn=>{
+    btn.addEventListener('click',function(e){
         e.preventDefault();
-
-        if (this.classList.contains('loading')) return;
+        if(this.classList.contains('loading')) return;
 
         this.classList.add('loading');
-
+        const fileName = this.dataset.file;
         const text = this.querySelector('.btn-text');
         text.textContent = "Скачивание...";
 
-        setTimeout(() => {
+        setTimeout(()=>{
+            const link = document.createElement('a');
+            link.href = fileName;
+            link.download = fileName;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
             text.textContent = "Скачать";
             this.classList.remove('loading');
-        }, 2500);
+        },2000);
     });
 });
